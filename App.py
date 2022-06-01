38fcc52f331c93ea0c7e8f71bdce249b438cac03
Donalson Javier <donalson1517@gmail.com>
2022-05-31 23:12:46 -0600

    Version de Prueba
    Se creo una lista de 140 colores en App.py, se creo un css interno en el layout para cambiar el color a toda la pagina, se creo un cartel con el color actual de la pagina y boton para cambiar de manera aleatoria la pagina

--- App.py
+++ App.py
@@ -1,16 +1,18 @@
 #Importacion de flask para crear y ejecutar sitio web, renderizar plantillas
 from flask import Flask, render_template
+from numpy import random
 
 #Instanciacion de flask dentro de la variable app
 app = Flask(__name__)
 
 #Variables a utilizar
-Colores = [];
+Colores = ["AliceBlue", "AntiqueWhite", "Aqua", "Aquamarine", "Azure", "Beige", "Bisque", "Black", "BlanchedAlmond", "Blue", "BlueViolet", "Brown", "BurlyWood", "CadetBlue", "Chartreuse", "Chocolate", "Coral", "CornflowerBlue", "Cornsilk", "Crimson", "Cyan", "DarkBlue", "DarkCyan", "DarkGoldenRod", "DarkGrey", "DarkGreen", "DarkKhaki", "DarkMagenta", "DarkOliveGreen", "Darkorange", "DarkOrchid", "DarkRed", "DarkSalmon", "DarkSeaGreen", "DarkSlateBlue", "DarkSlateGrey", "DarkTurquoise", "DarkViolet", "DeepPink", "DeepSkyBlue", "DimGray", "DodgerBlue", "FireBrick", "FloralWhite", "ForestGreen", "Fuchsia", "Gainsboro", "GhostWhite", "Gold", "GoldenRod", "Grey", "Green", "GreenYellow", "HoneyDew", "HotPink", "IndianRed", "Indigo", "Ivory", "Khaki", "Lavender", "LavenderBlush", "LawnGreen", "LemonChiffon", "LightBlue", "LightCoral", "LightCyan", "LightGoldenRodYellow", "LightGrey", "LightGreen", "LightPink", "LightSalmon", "LightSeaGreen", "LightSkyBlue", "LightSlateGrey", "LightSteelBlue", "LightYellow", "Lime", "LimeGreen", "Linen", "Magenta", "Maroon", "MediumAquaMarine", "MediumBlue", "MediumOrchid", "MediumPurple", "MediumSeaGreen", "MediumSlateBlue", "MediumSpringGreen", "MediumTurquoise", "MediumVioletRed", "MidnightBlue", "MintCream", "MistyRose", "Moccasin", "NavajoWhite", "Navy", "OldLace", "Olive", "OliveDrab", "Orange", "OrangeRed", "Orchid", "PaleGoldenRod", "PaleGreen", "PaleTurquoise", "PaleVioletRed", "PapayaWhip", "PeachPuff", "Peru", "Pink", "Plum", "PowderBlue", "Purple", "Red", "RosyBrown", "RoyalBlue", "SaddleBrown", "Salmon", "SandyBrown", "SeaGreen", "SeaShell", "Sienna", "Silver", "SkyBlue", "SlateBlue", "SlateGrey", "Snow", "SpringGreen", "SteelBlue", "Tan", "Teal", "Thistle", "Tomato", "Turquoise", "Violet", "Wheat", "White", "WhiteSmoke", "Yellow", "YellowGreen"]
 
 #Creacion de la ruta principal del sitio web y texto de prueba
 @app.route('/')
 def Inicio():
-    return render_template('Color.html', Colores = Colores)
+    Aleatorio = random.randint(140)
+    return render_template('Color.html', Colores = Colores , Aleatorio=Aleatorio)
 
 #Inializacion del sitio web para correr(modo debug encendido para detectar cambios)
 if __name__ == '__main__':
